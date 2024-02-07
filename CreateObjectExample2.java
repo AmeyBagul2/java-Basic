531b82052d8f1c555f69b25513b4b917534b2fd8
AmeyBagul2 <156235202+AmeyBagul2@users.noreply.github.com>
2024-02-07 23:57:06 +0530

    Create CreateObjectExample2.java

---
+++ CreateObjectExample2.java
@@ -0,0 +1,15 @@
+CreateObjectExample2.java
+
+public class CreateObjectExample2  
+{  
+//constructor of the class    
+CreateObjectExample2()    
+{    
+System.out.println("Welcome to javaTpoint");    
+}    
+public static void main(String[] args)   
+{    
+//creating an object using new keyword   
+CreateObjectExample2 obj = new CreateObjectExample2();   
+}    
+}   
